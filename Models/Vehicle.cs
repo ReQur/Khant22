25492aa742e7984ee4fed4b053b4d9ab1d757254
Danil Khabibullin <dkhabibullin@cloudlinux.com>
2022-12-03 19:43:50 +0500

    Upgrade vehicle service

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -3,12 +3,12 @@
     public class IVehicle
     {
         public uint vehicleId { get; set; }
-        public uint organizationId { get; set; }
+        public Organization organization { get; set; }
         public string model { get; set; }
         public string vehicleNumber { get; set; }
-        public uint vehicleTypeId { get; set; }
-        public uint serviceTypeId { get; set; }
-        public uint vehicleTypeExtId { get; set; }
+        public vehicleType vehicleType { get; set; }
+        public serviceType serviceType { get; set; }
+        public vehicleTypeExt vehicleTypeExt { get; set; }
         public string vehicleChars { get; set; }
         public string country { get; set; }
         public string fuelType { get; set; }
