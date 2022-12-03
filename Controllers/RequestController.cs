8b0893838e2495f2e755c74f39c5eb856269a96c
Evgeniy Berezin <79792471+FatherRtd@users.noreply.github.com>
2022-12-03 19:49:29 +0500

    requests

--- Controllers/RequestController.cs
+++ Controllers/RequestController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using dotnetserver.Models;
+using dotnetserver.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace dotnetserver.Controllers
 {
@@ -8,5 +12,29 @@ namespace dotnetserver.Controllers
 	[Route("api/[controller]")]
 	public class RequestController : ControllerBase
 	{
+		private readonly IRequestService _RequestService;
+
+		public RequestController(IRequestService RequestService)
+		{
+			_RequestService = RequestService;
+		}
+
+		[HttpGet(nameof(GetRequests) + "/organizationId")]
+		public Task<IEnumerable<Request>> GetRequests(int organizationId)
+		{
+			return default;
+		}
+
+		[HttpGet(nameof(GetRequests))]
+		public Task<IEnumerable<Request>> GetRequests()
+		{
+			return default;
+		}
+
+		[HttpPost(nameof(EditRequest))]
+		public Task<Request> EditRequest([FromBody] Request request)
+		{
+			return default;
+		}
 	}
 }
---
+++ Services/RequestService.cs
@@ -0,0 +1,31 @@
+﻿using dotnetserver.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace dotnetserver.Services
+{
+	public interface IRequestService
+	{
+		Task<IEnumerable<Request>> GetRequests(int organizationId);
+		Task<IEnumerable<Request>> GetRequests();
+		Task<Request> EditRequest(Request request);
+	}
+
+	public class RequestService : IRequestService
+	{
+		public Task<IEnumerable<Request>> GetRequests(int organizationId)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<IEnumerable<Request>> GetRequests()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<Request> EditRequest(Request request)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
