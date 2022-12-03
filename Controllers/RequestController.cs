9d164f3923f0c38e0d7c637122bca693a3a27bd4
Evgeniy Berezin <79792471+FatherRtd@users.noreply.github.com>
2022-12-04 04:44:04 +0500

    added QRcode костыль :DDD

--- Controllers/RequestController.cs
+++ Controllers/RequestController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace dotnetserver.Controllers
 {
 	[ApiController]
-	[Authorize]
+	//[Authorize]
 	[Route("api/[controller]")]
 	public class RequestController : ControllerBase
 	{
@@ -41,6 +41,30 @@ namespace dotnetserver.Controllers
 		public async Task<Request> AddRequest([FromBody] Request request)
 		{
 			return await _requestService.AddRequest(request);
+		}
+
+		[HttpGet(nameof(GetQrCode) + "/requestId")]
+		public async Task<ActionResult> GetQrCode(int requestId)
+		{
+			var result =  await _requestService.GetQrCode(requestId);
+			if (result == "")
+			{
+				return BadRequest();
+			}
+
+			return Ok(result);
+		}
+
+		[HttpGet(nameof(DecodeQrCode) + "/encoded")]
+		public async Task<ActionResult<Request>> DecodeQrCode(string encoded)
+		{
+			var result = await _requestService.DecodeQrCode(encoded);
+			if (result == null)
+			{
+				return BadRequest();
+			}
+
+			return Ok(result);
 		}
 	}
 }
--- Services/RequestService.cs
+++ Services/RequestService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using dotnetserver.Models;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Dapper;
 
@@ -12,6 +15,8 @@ namespace dotnetserver.Services
 		Task<IEnumerable<Request>> GetRequests();
 		Task<Request> AddRequest(Request request);
 		Task<Request> EditRequest(Request request);
+		Task<string> GetQrCode(int requestId);
+		Task<Request> DecodeQrCode(string hash);
 	}
 
 	public class RequestService : WithDbAccess, IRequestService
@@ -84,10 +89,9 @@ namespace dotnetserver.Services
 
 		public async Task<Request> AddRequest(Request request)
 		{
-			var query = "insert into request (status, userId, vehicleId, organizationId, comment) " +
-			            "values (@Status, @UserId, @VehicleId, @OrganizationId, @comment)";
+			var query = "insert into request (status, userId, vehicleId, organizationId, comment) values (@Status, @UserId, @VehicleId, @OrganizationId, @comment)";
 			var parameters = new { Status = request.status, UserId = request.user.userId, 
-				OrganizationId = request.organization.organizationId, Comment = request.comment};
+				VehicleId = request.vehicle.vehicleId, OrganizationId = request.organization.organizationId, Comment = request.comment};
 
 			using (var db = _context.GenericConnection())
 			{
@@ -117,6 +121,76 @@ namespace dotnetserver.Services
 				{
 					var result = await db.QueryAsync(query, parameters);
 					return request;
+				}
+				catch (Exception e)
+				{
+					throw;
+				}
+			}
+		}
+
+		public async Task<string> GetQrCode(int requestId)
+		{
+			var query = "select * from request where requestId = @Id";
+			var parameters = new { Id = requestId };
+
+			using (var db = _context.GenericConnection())
+			{
+				db.Open();
+				try
+				{
+					var result = await db.QueryAsync<Request>(query, parameters);
+					if (result.Count() != 0)
+					{
+						var stringToEncode = "request" + result.First().requestId.ToString();
+						return stringToEncode;
+					}
+					else
+					{
+						return "";
+					}
+				}
+				catch (Exception e)
+				{
+					throw;
+				}
+			}
+		}
+
+		public async Task<Request> DecodeQrCode(string encodedString)
+		{
+			var requestId = encodedString.Substring("request".Length);
+			var query = @"select r.*, u.*, o.*, v.*
+							from request r
+							left join user u on r.userId = u.userId
+							left join organization o on r.organizationId = o.organizationId
+							left join vehicle v on r.vehicleId = v.vehicleId
+							where requestId = @Id;";
+			var parameters = new { Id = requestId };
+
+			using (var db = _context.GenericConnection())
+			{
+				db.Open();
+				try
+				{
+					var result = await db.QueryAsync<Request, User, Organization, Vehicle, Request>(
+						query,
+						(req, user, org, veh) =>
+						{
+							req.user = user;
+							req.organization = org;
+							req.vehicle = veh;
+							return req;
+						},parameters, splitOn: "userId, organizationId, vehicleId"
+					);
+					if (result.Count() != 0)
+					{
+						return result.First();
+					}
+					else
+					{
+						return null;
+					}
 				}
 				catch (Exception e)
 				{
