7951683a5263e3a3a1c069a1aea613bfd6ad2def
Danil Khabibullin <dkhabibullin@cloudlinux.com>
2022-12-03 17:38:28 +0500

    Add models

---
+++ Models/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿namespace dotnetserver.Models
+{
+    public class IVehicle
+    {
+        public uint vehicleId { get; set; }
+        public uint organizationId { get; set; }
+        public string model { get; set; }
+        public string vehicleNumber { get; set; }
+        public uint vehicleTypeId { get; set; }
+        public uint serviceTypeId { get; set; }
+        public uint vehicleTypeExtId { get; set; }
+        public string vehicleChars { get; set; }
+        public string country { get; set; }
+        public string fuelType { get; set; }
+        public bool subOrganization { get; set; }
+        public string ownershipType { get; set; }
+    }
+
+    public class Vehcle: IVehicle
+    {
+    }
+
+}
