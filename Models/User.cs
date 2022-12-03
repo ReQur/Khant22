1463a2bd22901570b57279a2a2716006fb1b1b3b
Evgeniy Berezin <79792471+FatherRtd@users.noreply.github.com>
2022-12-03 17:39:49 +0500

    Models

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace dotnetserver.Models
+{
+	public class User
+    {
+	    public uint userId { get; set; }
+	    public string firstName { get; set; }
+	    public string lastName { get; set; }
+	    public string Login { get; set; }
+	    public string password { get; set; }
+	    public uint OrganizationId { get; set; }
+	    public string JobTitle { get; set; }
+    }
+}
